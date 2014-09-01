68204e7980b16f3dfd03b5905074792d7480db23
Dawid Zegar <davvvid1@gmail.com>
2014-09-01 14:08:48 +0200

    finish exercise2

--- app/AnnouncementListCtrl.js
+++ app/AnnouncementListCtrl.js
@@ -1,7 +1,12 @@
 (function () {
     'use strict';
     function AnnouncementListCtrl(AnnouncementDAO) {
+        var hw = this;
 
+
+        AnnouncementDAO.query().then(function (data) {
+            hw.posts = data;
+        })
     }
 
     var module = angular.module("exerciseApp");
