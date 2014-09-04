f52792824a3437b06895685bb640fc4d10ee700d
Dawid Zegar <davvvid1@gmail.com>
2014-09-04 10:02:18 +0200

    finish exercise7

--- app/scripts/controllers/typeahead.js
+++ app/scripts/controllers/typeahead.js
@@ -2,13 +2,19 @@
 
 angular.module('itcApp').controller('TypeaheadCtrl', function ($filter,ContactDAO) {
 
+    var ctrl = this;
     this.selected = undefined;
 
+
     this.getContacts=function(typedValue){
-	// need to write this function body
-    }
+        ContactDAO.getAll().then(function (data) {
+            ctrl.variable = data[0];
+        });
+    };
 
     this.selectContact=function(item,model,label){
         this.selected=item;
-    }
+    };
+
+
 });
