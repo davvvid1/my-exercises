039f4b71aa7e4477afa568134c4c50d9be0820df
Dawid Zegar <davvvid1@gmail.com>
2014-09-05 12:12:20 +0200

    modify description in exercise1

--- test/spec/BlogPostCtrl.spec.js
+++ test/spec/BlogPostCtrl.spec.js
@@ -1,26 +1,22 @@
-(function () {
+describe("exercise1", function () {
+    beforeEach(module('exerciseApp'));
+    var controller;
 
-    describe("ShoppingCartCtrl", function () {
-        beforeEach(module('exerciseApp'));
-        var controller;
+    beforeEach(inject(function ($controller) {
+        controller = $controller('BlogPostCtrl', {});
+    }));
 
-        beforeEach(inject(function ($controller) {
-            controller = $controller('BlogPostCtrl', {});
-        }));
-
-        describe("BlogPostCtrl", function () {
-            it("should defined posts", function () {
-                expect(controller.posts).toBeDefined();
-            });
-            it("should be instance of Array", function () {
-                expect(controller.posts instanceof Array).toBeTruthy();
-            });
-            it("should 'posts' has 'id', 'name' and 'pets' properties", function () {
-                expect(controller.posts[0].id).toBeDefined();
-                expect(controller.posts[0].name).toBeDefined();
-                expect(controller.posts[0].pets).toBeDefined();
-            });
+    describe("BlogPostCtrl", function () {
+        it("should defined posts", function () {
+            expect(controller.posts).toBeDefined();
+        });
+        it("should be instance of Array", function () {
+            expect(controller.posts instanceof Array).toBeTruthy();
+        });
+        it("should 'posts' has 'id', 'name' and 'pets' properties", function () {
+            expect(controller.posts[0].id).toBeDefined();
+            expect(controller.posts[0].name).toBeDefined();
+            expect(controller.posts[0].pets).toBeDefined();
         });
     });
-
-})();
+});//
