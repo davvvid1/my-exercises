55b7ff76b1b81894432e480a3c20479d00c18bce
Dawid Zegar <davvvid1@gmail.com>
2014-09-05 09:39:12 +0200

    add tests to exercise1

---
+++ test/spec/BlogPostCtrl.spec.js
@@ -0,0 +1,26 @@
+(function () {
+
+    describe("ShoppingCartCtrl", function () {
+        beforeEach(module('exerciseApp'));
+        var controller;
+
+        beforeEach(inject(function ($controller) {
+            controller = $controller('BlogPostCtrl', {});
+        }));
+
+        describe("BlogPostCtrl", function () {
+            it("should defined posts", function () {
+                expect(controller.posts).toBeDefined();
+            });
+            it("should be instance of Array", function () {
+                expect(controller.posts instanceof Array).toBeTruthy();
+            });
+            it("should 'posts' has 'id', 'name' and 'pets' properties", function () {
+                expect(controller.posts[0].id).toBeDefined();
+                expect(controller.posts[0].name).toBeDefined();
+                expect(controller.posts[0].pets).toBeDefined();
+            });
+        });
+    });
+
+})();
