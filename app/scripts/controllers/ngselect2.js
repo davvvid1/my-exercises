3c952f27bad0cceefe959fd797c2bcddc13e997d
Dawid Zegar <davvvid1@gmail.com>
2014-09-09 09:50:18 +0200

    finish exercise10

--- app/scripts/controllers/ngselect2.js
+++ app/scripts/controllers/ngselect2.js
@@ -14,47 +14,26 @@ angular.module('ngselect2App').controller('Ngselect2Ctrl', function ($scope, $re
     this.artistModel = [];
 
 
-    console.log(this.query);
-
-
-    $scope.$watch(function () {
-        return ctrl.query;
-    }, function () {
-        //console.log(ctrl.query);
-        ctrl.search(ctrl.query);
-        //console.log(ctrl.artists);
-    }, true);
-
-
-    this.search = function (que) {
-        if (!que) {
-            console.log('return');
-            return;
-        }
-        Artist.query(que).then(function (data) {
-            ctrl.artists = data.artist;
-        });
-    }
-
-
-    this.select2options = {
+    ctrl.select2options = {
         multiple: true,
-        simple_tags: true,
-        tags: []  // Can be empty list.
+        //simple_tags: true,
+        minimumInputLength: 1,
+        query: function (que) {
+            Artist.query(que.term).then(function (data) {
+                ctrl.artists = data.artist.map(function (a, b) {
+                    var obj = {};
+                    obj.id = b + 1;
+                    obj.text = a.name;
+                    return obj;
+                });
+            });
+            var data = {
+                results: ctrl.artists
+            };
+            que.callback(data);
+        }
+
     };
-
-
-    $("#sel").on('select2-highlight', function (e) {
-
-
-        ctrl.search(e.val);
-        console.log(ctrl.artists);
-        ctrl.artists.every(function (a, b) {
-            console.log(a.name);
-            ctrl.select2options.tags[b] = a.name;
-            return true;
-        })
-    });
 
 
 });
